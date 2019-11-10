cdcc3560c4a2b0654ee7db4d86eee103e5b8aaaa
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2019-11-10 15:42:56 -0600

    Creating body component

---
+++ src/components/Body.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+import { minHeight } from '@material-ui/system';
+
+export interface BodyProps {
+  
+}
+
+const useStyles = makeStyles({
+  root: {
+    margin: '5% 25%',
+    backgroundColor: "white",
+    padding: "20px",
+    minHeight: "600px",
+  },
+});
+ 
+const Body: React.SFC<BodyProps> = () => {
+  const classes = useStyles();
+  return (
+  <div className={classes.root}>
+    <Typography variant="h5">
+      Real time dashboard by Eder Ramírez
+    </Typography>
+  </div>
+  );
+}
+ 
+export default Body;
