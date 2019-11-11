81d767cd457aaa096d8eb6f744bd23594394d9a3
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2019-11-10 18:14:17 -0600

    Creating metrics selector component

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 import { useQuery } from '@apollo/react-hooks';
 import { getMetrics } from '../apollo/queries';
+import MetricsSelector from './MetricsSelector';
 const data = [
   {name: 'Page A', uv: 400, pv: 2400, amt: 2400},
   {name: 'Page A', uv: 500, pv: 2400, amt: 2400},
@@ -18,12 +19,27 @@ export interface ChartProps {
  
 const Chart: React.SFC<ChartProps> = () => {
   const { data: metricsData, loading: metricsLoading, error: metricsError } = useQuery(getMetrics);
-
-  if (metricsLoading) return <p>Loading...</p>;
+  //let metricsInitialState: object = {}
+  const [metricsInitialState, setMetricsInitialState] = React.useState({});
+  
+  React.useEffect(() => {
+    console.log("TCL: metricsData", metricsData)
+    if(metricsData && metricsData.getMetrics.length > 0){
+      const res = metricsData.getMetrics.reduce((o: object, key: number) => ({ ...o, [key]: false}), {})
+      setMetricsInitialState(res);
+      console.log("TCL: metricsInitialState", metricsInitialState)
+    }
+  }, [metricsData])
+  
+  if (metricsLoading || Object.keys(metricsInitialState).length === 0 ) return <p>Loading...</p>;
   if (metricsError) return <p>Error :(</p>;
-
+    
+    
+    
+    console.log("TCL: metricsInitialState", metricsInitialState)
   return (
     <div>
+      <MetricsSelector metrics={metricsData.getMetrics} metricsInitialState={metricsInitialState}/>
       <LineChart width={900} height={500} data={data} margin={{ top: 50, right: 20, bottom: 5, left: 0 }}>
         <Line type="monotone" dataKey="uv" stroke="#8884d8" />
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
